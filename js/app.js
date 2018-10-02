c01483113aaf47ff86268cac896207980a06750c
xs9734 <42820350+xs9734@users.noreply.github.com>
2018-10-01 20:40:42 -0700

    footer placeholder

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation()
+
+$('#primary-menu').on(
+    'show.zf.dropdownmenu', function() {
+      var dropdown = $(this).find('.is-dropdown-submenu');
+      dropdown.css('display', 'none');
+      dropdown.fadeIn('slow');
+  });
+  $('#primary-menu').on(
+    'hide.zf.dropdownmenu', function() {
+      var dropdown = $(this).find('.is-dropdown-submenu');
+      dropdown.css('display', 'inherit');
+      dropdown.fadeOut('slow');
+  });
+  
